0f6e3fa4ed47ba33ffcdab54cd4a0d68ab7a9db5
Brendan Mulkern <brenjoe.mulkern@gmail.com>
2022-04-23 14:34:39 -0600

    adding cross validation to svm

--- svm.py
+++ svm.py
@@ -6,6 +6,7 @@ from sklearn import svm
 import sklearn.model_selection as model_selection
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_val_score
 import warnings
 
 def build_dataframe(csv_file):
@@ -44,20 +45,25 @@ def main():
     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, train_size=0.80, test_size=0.20, random_state=101)
 
     rbf = svm.SVC(kernel='rbf', gamma=0.5, C=0.1).fit(X_train, y_train)
-    poly = svm.SVC(kernel='poly', degree=3, C=1).fit(X_train, y_train)
+    rbf_cv = svm.SVC(kernel='rbf', gamma=0.5, C=0.1)
+    scores = cross_val_score(rbf_cv, X, y, cv=5)
 
-    poly_pred = poly.predict(X_test)
     rbf_pred = rbf.predict(X_test)
-
-    poly_accuracy = accuracy_score(y_test, poly_pred)
-    poly_f1 = f1_score(y_test, poly_pred, average='weighted')
-    print('Accuracy (Polynomial Kernel): ', "%.2f" % (poly_accuracy*100))
-    print('F1 (Polynomial Kernel): ', "%.2f" % (poly_f1*100))
 
     rbf_accuracy = accuracy_score(y_test, rbf_pred)
     rbf_f1 = f1_score(y_test, rbf_pred, average='weighted')
     print('Accuracy (RBF Kernel): ', "%.2f" % (rbf_accuracy*100))
     print('F1 (RBF Kernel): ', "%.2f" % (rbf_f1*100))
+    print('CV Scores: ', scores)
+
+    # poly = svm.SVC(kernel='poly', degree=3, C=1).fit(X_train, y_train)
+
+    # poly_pred = poly.predict(X_test)
+
+    # poly_accuracy = accuracy_score(y_test, poly_pred)
+    # poly_f1 = f1_score(y_test, poly_pred, average='weighted')
+    # print('Accuracy (Polynomial Kernel): ', "%.2f" % (poly_accuracy*100))
+    # print('F1 (Polynomial Kernel): ', "%.2f" % (poly_f1*100))
 
 if __name__ == "__main__":
     main()
